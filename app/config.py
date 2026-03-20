84c9477159f15489baadd55e2d9f408f7d16786c
Sequence-Zero <christopher.keenan1@snhu.edu>
2026-03-20 16:13:38 -0400

    fixed config issues

--- app/config.py
+++ app/config.py
@@ -1,7 +1,7 @@
 import os
 
 class Config:
-    SECRET_KEY = os.getenv("SECRET_KEY", "dev-secret-change-me")
+    SECRET_KEY = os.getenv("SECRET_KEY", "fallback_secret")
     SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL", "sqlite:///sensorstream.db")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     ADMIN_TOKEN = os.getenv("ADMIN_TOKEN", "dev-admin-token")
